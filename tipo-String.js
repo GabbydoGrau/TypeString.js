542e3a98dd8883b39c61c939fb5a0d8f1e5fcf7b
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-05-24 12:06:08 -0300

    tipo-String.js

---
+++ tipo-String.js
@@ -0,0 +1,23 @@
+// tipoString 
+// cadeia de caracteres (simbolos)
+// exemplo:
+// frases
+// textos ...
+
+const string1 = "Jose";
+const string2 = "Valin";
+
+// concatenar
+// juntar 2 strings
+const nome = string1 + string2; 
+
+console.log(nome); 
+
+// exemplo
+const nome2 = "jose";
+const sobrenome = "Valim";
+const idade = 30;
+
+console.log("nome:" + nome2);
+console.log("sobrenome:" + sobrenome);
+console.log("idade:" + idade);
